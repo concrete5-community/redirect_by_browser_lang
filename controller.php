bf239ea300ac9a0020e3009f5895d40ce6481fb2
Michele Locati <michele@locati.it>
2024-05-20 16:16:42 +0200

    Version 1.0.0

--- controller.php
+++ controller.php
@@ -22,7 +22,7 @@ class Controller extends Package
      *
      * @var string
      */
-    protected $pkgVersion = '0.0.1';
+    protected $pkgVersion = '1.0.0';
 
     /**
      * The minimum concrete5/ConcreteCMS version.
